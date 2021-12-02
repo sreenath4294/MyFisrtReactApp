6ee9870a4b302294a59b841ea30bd70e5e0a3485
Sreenath <sreenath4294@gmail.com>
2021-12-02 16:06:16 +0530

    create component expenses-date and learn props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,13 @@
 import "../styles/ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
-function ExpenseItem() {
-  const expenseDate = new Date(2021, 12, 1);
-  const expenseTitle = "Mobile purchase";
-  const expenseAmount = 11499;
-
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">₹ {expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">₹ {props.amount}</div>
       </div>
     </div>
   );
